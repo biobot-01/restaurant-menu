3600eba2ae43d87f27200fe3a7dd09aa2abd66d9
Kevin Michael <biobot.mike@gmail.com>
2019-03-22 14:42:27 +0200

    Add all imports that will be used

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String
+
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy.orm import relationship
+
+from sqlalchemy import create_engine
