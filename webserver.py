5d5878ef3824b4c825e851f097462af6b9c67665
Kevin Michael <biobot.mike@gmail.com>
2019-03-23 01:45:09 +0200

    Create a server instance in main function

--- webserver.py
+++ webserver.py
@@ -2,7 +2,17 @@
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+
 def main():
+    try:
+        port = 8000
+        server_address = ('', port)
+        httpd = HTTPServer(server_address, WebServerHandler)
+        print("Web server running on port {}".format(port))
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("^C entered, stopping web server...")
+        httpd.socket.close()
 
 
 if __name__ == '__main__':
