3af7b9dab5cbcfb14c717214e42eea05d034c3b2
Kevin Michael <biobot.mike@gmail.com>
2019-03-22 15:13:05 +0200

    Add MenuItem class for menu item table

--- database_setup.py
+++ database_setup.py
@@ -19,6 +19,19 @@ class Restaurant(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+
 # Insert at E_O_F #
 engine = create_engine('sqlite:///restaurantmenu.db')
 
