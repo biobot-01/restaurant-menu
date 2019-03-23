47c1e898fdd7024b7fbb2681e2d18496fffbf945
Kevin Michael <biobot.mike@gmail.com>
2019-03-23 15:32:13 +0200

    Add web server class to handle get requests

--- webserver.py
+++ webserver.py
@@ -3,7 +3,37 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
+class WebServerHandler(BaseHTTPRequestHandler):
+    """The HTTP method this web server can handle"""
+
+    def do_GET(self):
+        try:
+            if self.path.endswith('/hello'):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = '''
+                    <html>
+                      <head>
+                        <title>Restaurant Menu</title>
+                      </head>
+                      <body>
+                        <h1>Hello!</h1>
+                      </body>
+                    </html>
+                '''
+
+                self.wfile.write(output)
+                print(output)
+                return
+        except IOError:
+            self.send_error(404, "File Not Found {}".format(self.path))
+
+
 def main():
+    """Main function for this file is to create a web server instance"""
+
     try:
         port = 8000
         server_address = ('', port)
