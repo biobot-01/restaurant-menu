9969f3deda8eab25edf348b137c8f18991e9e6ae
Kevin Michael <biobot.mike@gmail.com>
2019-03-26 18:47:13 +0200

    Add project.py file and setup flask hello world app

---
+++ project.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/hello')
+def hello_world():
+    return "Hello World"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='127.0.0.1', port=8000)
