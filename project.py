afe88453da2c3633e866b2facdf8f605e85701de
Kevin Michael <biobot.mike@gmail.com>
2019-03-26 20:32:57 +0200

    Add routes to create, edit and delete menu items with function biolerplate for each

--- project.py
+++ project.py
@@ -42,6 +42,21 @@ def restaurant_menu(restaurant_id):
     return output
 
 
+@app.route('/restaurants/<int:restaurant_id>/new')
+def new_menu_item(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+def edit_menu_item(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+def delete_menu_item(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='127.0.0.1', port=8000)
