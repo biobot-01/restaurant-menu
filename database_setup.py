4564c447fb1cfc8c41f2501e6fab582894140028
Kevin Michael <biobot.mike@gmail.com>
2019-03-27 21:20:30 +0200

    Add support to request JSON objects for an API endpoint

--- database_setup.py
+++ database_setup.py
@@ -29,6 +29,17 @@ class MenuItem(Base):
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
 
+    @property
+    def serialize(self):
+        # Returns JSON objects in serializeable format
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course
+        }
+
 
 # Insert at E_O_F #
 engine = create_engine('sqlite:///restaurantmenu.db')
--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import (Flask, render_template, request, redirect, url_for,
+                   flash, jsonify)
 
 from database_setup import Base, Restaurant, MenuItem
 
@@ -19,6 +20,22 @@ Base.metadata.bind = engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Making an API Endpoint (GET Request)
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON')
+def restaurant_menu_json(restaurant_id):
+    items = session.query(MenuItem).filter_by(
+        restaurant_id=restaurant_id
+    ).all()
+
+    return jsonify(MenuItems=[i.serialize for i in items])
+
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON')
+def menu_item_json(restaurant_id, menu_id):
+    item = session.query(MenuItem).filter_by(id=menu_id).one()
+
+    return jsonify(MenuItem=item.serialize)
+
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/menu')
@@ -28,7 +45,7 @@ def restaurant_menu(restaurant_id):
     ).one()
     items = session.query(MenuItem).filter_by(
         restaurant_id=restaurant_id
-    )
+    ).all()
 
     return render_template('menu.html', restaurant=restaurant, items=items)
 
