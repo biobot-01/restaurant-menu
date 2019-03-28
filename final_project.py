16db4f39fc894433a3bbdf79bb21ab4316d09781
Kevin Michael <biobot.mike@gmail.com>
2019-03-28 14:10:11 +0200

    Add templates and data to be returned by each route function

--- final_project.py
+++ final_project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,48 +8,53 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/restaurants')
 def show_restaurants():
-    return "Show all restaurant names"
+    return render_template('restaurants.html', restaurants=restaurants)
 
 
 @app.route('/restaurant/new')
 def new_restaurant():
-    return "Create a restaurant"
+    return render_template('new-restaurant.html')
 
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def edit_restaurant(restaurant_id):
-    return "Edit restaurant with id {}".format(restaurant_id)
+    return render_template('edit-restaurant.html', restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def delete_restaurant(restaurant_id):
-    return "Delete restaurant with id {}".format(restaurant_id)
+    return render_template(
+        'delete-restaurant.html',
+        restaurant_id=restaurant_id
+    )
 
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def show_restaurant_menu(restaurant_id):
-    return "Show a restaurant menu"
+    return render_template('menu.html', restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def new_menu_item(restaurant_id):
-    return "Create a menu item for restaurant with id {}".format(restaurant_id)
+    return render_template('new-menu-item.html', restaurant_id=restaurant_id)
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def edit_menu_item(restaurant_id, menu_id):
-    return "Edit menu item with id {} for restaurant with id {}".format(
-        menu_id,
-        restaurant_id
+    return render_template(
+        'edit-menu-item.html',
+        restaurant_id=restaurant_id,
+        menu_id=menu_id
     )
 
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def delete_menu_item(restaurant_id, menu_id):
-    return "Delete menu item with id {} for restaurant with id {}".format(
-        menu_id,
-        restaurant_id
+    return render_template(
+        'delete-menu-item.html',
+        restaurant_id=restaurant_id,
+        menu_id=menu_id
     )
 
 
