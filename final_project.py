a1a3d2460baadbbb213daadf3589e3ac9233e2a0
Kevin Michael <biobot.mike@gmail.com>
2019-03-30 04:33:11 +0200

    Add Create functionality for restaurant menu

--- final_project.py
+++ final_project.py
@@ -97,8 +97,31 @@ def show_restaurant_menu(restaurant_id):
     )
 
 
-@app.route('/restaurant/<int:restaurant_id>/menu/new')
+@app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
 def new_menu_item(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        price = request.form['price']
+        course = request.form['course']
+        new_item = MenuItem(
+            name=name,
+            description=description,
+            price=price,
+            course=course,
+            restaurant_id=restaurant_id
+        )
+
+        session.add(new_item)
+        session.commit()
+
+        return redirect(url_for(
+            'show_restaurant_menu',
+            restaurant_id=restaurant_id
+        ))
+
     return render_template('new-menu-item.html', restaurant=restaurant)
 
 
